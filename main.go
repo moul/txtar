fca4e103cafd7278033a88572ee317ff8e7fb6cc
moul <94029+moul@users.noreply.github.com>
2025-01-24 09:02:29 +0100

    feat: add support for stdin list of files
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -19,64 +20,91 @@ func main() {
 	)
 	flag.Parse()
 
-	dirs := flag.Args()
-	if len(dirs) == 0 {
-		fmt.Fprintln(os.Stderr, "at least one target directory must be specified")
-		os.Exit(1)
-	}
-
 	var ar txtar.Archive
+	dirs := flag.Args()
 
-	for _, dir := range dirs {
-		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			base := filepath.Base(path)
-
-			if info.IsDir() {
-				if len(base) > 1 && base[0] == '.' && base[1] != '.' && base[1] != '/' {
-					return filepath.SkipDir
-				}
-				return nil
-			}
-
-			if len(base) > 0 && base[0] == '.' {
-				return nil
-			}
-
+	if len(dirs) == 0 {
+		// Read files from stdin
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			path := scanner.Text()
 			data, err := ioutil.ReadFile(path)
 			if err != nil {
-				return err
+				continue // Skip files we can't read
 			}
 
 			if !utf8.Valid(data) {
 				// TODO: log skipped files
-				return nil
+				continue
 			}
 
 			p := filepath.ToSlash(path)
 			if *flagListOnly {
 				fmt.Println(strings.TrimPrefix(p, *flagStripPrefix))
-				return nil
+				continue
 			}
 
 			ar.Files = append(ar.Files, txtar.File{
 				Name: strings.TrimPrefix(p, *flagStripPrefix),
 				Data: data,
 			})
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "error reading stdin:", err)
+			os.Exit(1)
+		}
+	} else {
+		for _, dir := range dirs {
+			err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
 
-			return nil
-		})
-		if err != nil {
-			panic(err)
+				base := filepath.Base(path)
+
+				if info.IsDir() {
+					if len(base) > 1 && base[0] == '.' && base[1] != '.' && base[1] != '/' {
+						return filepath.SkipDir
+					}
+					return nil
+				}
+
+				if len(base) > 0 && base[0] == '.' {
+					return nil
+				}
+
+				data, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				if !utf8.Valid(data) {
+					// TODO: log skipped files
+					return nil
+				}
+
+				p := filepath.ToSlash(path)
+				if *flagListOnly {
+					fmt.Println(strings.TrimPrefix(p, *flagStripPrefix))
+					return nil
+				}
+
+				ar.Files = append(ar.Files, txtar.File{
+					Name: strings.TrimPrefix(p, *flagStripPrefix),
+					Data: data,
+				})
+
+				return nil
+			})
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 
 	if !*flagListOnly {
 		if len(ar.Files) == 0 {
-			fmt.Fprintln(os.Stderr, "no files found in target directories")
+			fmt.Fprintln(os.Stderr, "no files found")
 			os.Exit(1)
 		}
 		archived := string(txtar.Format(&ar))
