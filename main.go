9ec0817e71fb52ab305d5667b24a10a71958c4a7
moul <94029+moul@users.noreply.github.com>
2025-01-24 09:00:02 +0100

    feat: add support for multiple dirs
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- main.go
+++ main.go
@@ -19,62 +19,64 @@ func main() {
 	)
 	flag.Parse()
 
-	dir := flag.Arg(0)
-	if dir == "" {
-		fmt.Fprintln(os.Stderr, "target directory must be specified")
+	dirs := flag.Args()
+	if len(dirs) == 0 {
+		fmt.Fprintln(os.Stderr, "at least one target directory must be specified")
 		os.Exit(1)
 	}
 
 	var ar txtar.Archive
 
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		base := filepath.Base(path)
-
-		if info.IsDir() {
-			if len(base) > 1 && base[0] == '.' && base[1] != '.' && base[1] != '/' {
-				return filepath.SkipDir
+	for _, dir := range dirs {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
 			}
+
+			base := filepath.Base(path)
+
+			if info.IsDir() {
+				if len(base) > 1 && base[0] == '.' && base[1] != '.' && base[1] != '/' {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+
+			if len(base) > 0 && base[0] == '.' {
+				return nil
+			}
+
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			if !utf8.Valid(data) {
+				// TODO: log skipped files
+				return nil
+			}
+
+			p := filepath.ToSlash(path)
+			if *flagListOnly {
+				fmt.Println(strings.TrimPrefix(p, *flagStripPrefix))
+				return nil
+			}
+
+			ar.Files = append(ar.Files, txtar.File{
+				Name: strings.TrimPrefix(p, *flagStripPrefix),
+				Data: data,
+			})
+
 			return nil
-		}
-
-		if len(base) > 0 && base[0] == '.' {
-			return nil
-		}
-
-		data, err := ioutil.ReadFile(path)
-		if err != nil {
-			return err
-		}
-
-		if !utf8.Valid(data) {
-			// TODO: log skipped files
-			return nil
-		}
-
-		p := filepath.ToSlash(path)
-		if *flagListOnly {
-			fmt.Println(strings.TrimPrefix(p, *flagStripPrefix))
-			return nil
-		}
-
-		ar.Files = append(ar.Files, txtar.File{
-			Name: strings.TrimPrefix(p, *flagStripPrefix),
-			Data: data,
 		})
-
-		return nil
-	})
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	if !*flagListOnly {
 		if len(ar.Files) == 0 {
-			fmt.Fprintln(os.Stderr, "target directory is empty")
+			fmt.Fprintln(os.Stderr, "no files found in target directories")
 			os.Exit(1)
 		}
 		archived := string(txtar.Format(&ar))
